8cd40d75583b8cd40eb4c4f44acbb2735684455a
kKar1503 <isonice4572@gmail.com>
2022-02-13 11:39:50 +0800

    Cleaned up old codes

--- src/jprg_assignment/Test.java
+++ src/jprg_assignment/Test.java
@@ -35,46 +35,46 @@ public class Test {
             System.out.println(credential.getName());
         }
     }
-    
-    public static ArrayList<Student> initializeStudents() {
-        // Modules for Student 1
-        Module m1 = new Module("ST0501", "FED", 5, 75.0);
-        Module m2 = new Module("ST0502", "FOP", 6, 85.0);
-        Module m3 = new Module("ST2413", "FOC", 4, 77.0);
-        Module m4 = new Module("LC0860", "CAT", 2, 83.0);
-        Module m5 = new Module("LC0855", "CPR", 2, 90.0);
-        
-        // Modules for Student 2
-        Module m6 = new Module("ST0503", "BED", 5, 90.0);
-        Module m7 = new Module("ST0504", "MAD", 4, 59.0);
-        Module m8 = new Module("ST0509", "JPRG", 6, 82.0);
-        Module m9 = new Module("MS0105", "MATH", 4, 70.0);
-        Module m10 = new Module("LC0861", "NAT", 2, 75.0);
-        
-        // Modules for Student 3
-        Module m11 = new Module("ST0501", "FED", 5, 50.0);
-        Module m12 = new Module("ST0503", "BED", 5, 65.0);
-        Module m13 = new Module("ST0509", "JPRG", 6, 60.0);
-        Module m14 = new Module("ST0277", "DEUI", 3, 49.0);
-        Module m15 = new Module("LC0861", "NAT", 2, 70.0);
-        Module m16 = new Module("LC0855", "CPR", 2, 80.0);
-        
-        // Module ArrayLists for Students
-        ArrayList<Module> moduleList1 = new ArrayList<>(Arrays.asList(m1,m2,m3,m4,m5)); // Student 1
-        ArrayList<Module> moduleList2 = new ArrayList<>(Arrays.asList(m6,m7,m8,m9,m10)); // Student 2
-        ArrayList<Module> moduleList3 = new ArrayList<>(Arrays.asList(m11,m12,m13,m14,m15,m16)); // Student 3
-        
-        // Initialize Students
-        Student s1 = new Student("John Tan", "DCITP", 2123181, moduleList1);
-        Student s2 = new Student("James Lim", "DIT", 2134599, moduleList2);
-        Student s3 = new Student("Jimmy Loh", "DAAA", 2136034, moduleList3);
-        
-        ArrayList<Student> studentList = new ArrayList<>();
-        studentList.add(s1);        
-        studentList.add(s2);
-        studentList.add(s3);
-        return studentList;
-    }
+//    
+//    public static ArrayList<Student> initializeStudents() {
+//        // Modules for Student 1
+//        Module m1 = new Module("ST0501", "FED", 5, 75.0);
+//        Module m2 = new Module("ST0502", "FOP", 6, 85.0);
+//        Module m3 = new Module("ST2413", "FOC", 4, 77.0);
+//        Module m4 = new Module("LC0860", "CAT", 2, 83.0);
+//        Module m5 = new Module("LC0855", "CPR", 2, 90.0);
+//        
+//        // Modules for Student 2
+//        Module m6 = new Module("ST0503", "BED", 5, 90.0);
+//        Module m7 = new Module("ST0504", "MAD", 4, 59.0);
+//        Module m8 = new Module("ST0509", "JPRG", 6, 82.0);
+//        Module m9 = new Module("MS0105", "MATH", 4, 70.0);
+//        Module m10 = new Module("LC0861", "NAT", 2, 75.0);
+//        
+//        // Modules for Student 3
+//        Module m11 = new Module("ST0501", "FED", 5, 50.0);
+//        Module m12 = new Module("ST0503", "BED", 5, 65.0);
+//        Module m13 = new Module("ST0509", "JPRG", 6, 60.0);
+//        Module m14 = new Module("ST0277", "DEUI", 3, 49.0);
+//        Module m15 = new Module("LC0861", "NAT", 2, 70.0);
+//        Module m16 = new Module("LC0855", "CPR", 2, 80.0);
+//        
+//        // Module ArrayLists for Students
+//        ArrayList<Module> moduleList1 = new ArrayList<>(Arrays.asList(m1,m2,m3,m4,m5)); // Student 1
+//        ArrayList<Module> moduleList2 = new ArrayList<>(Arrays.asList(m6,m7,m8,m9,m10)); // Student 2
+//        ArrayList<Module> moduleList3 = new ArrayList<>(Arrays.asList(m11,m12,m13,m14,m15,m16)); // Student 3
+//        
+//        // Initialize Students
+//        Student s1 = new Student("John Tan", "DCITP", 2123181, moduleList1);
+//        Student s2 = new Student("James Lim", "DIT", 2134599, moduleList2);
+//        Student s3 = new Student("Jimmy Loh", "DAAA", 2136034, moduleList3);
+//        
+//        ArrayList<Student> studentList = new ArrayList<>();
+//        studentList.add(s1);        
+//        studentList.add(s2);
+//        studentList.add(s3);
+//        return studentList;
+//    }
     
 //    public static void main(String[] args) {
 //        ObjectIO.studentSerialization(initializeStudents());
